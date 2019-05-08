f44fafdae773d1101a9d0c4ae7f2cdb2e4feb776
Your Name <you@example.com>
2019-05-08 14:33:40 +0300

    git App

---
+++ app.js
@@ -0,0 +1,13 @@
+const github = require('./github');
+
+const username = process.argv[2];
+
+github.getRepos(username, (error, repos) => {
+    if (error) return console.error(`Ошибка: ${error.message}`);
+
+    repos.forEach(repo => {
+        console.log(repo.name);
+    });
+});
+
+console.log(process.argv);
